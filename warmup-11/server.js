5fa33156899483928ab0bd0a2b2310c8d29e8c27
Cory Henderson <chenderson19899@gmail.com>
2019-05-09 17:40:48 -0700

    warmup-11

---
+++ warmup-11/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const app= require('express')();
+const router = require('./routes/routes.js');
+const errorHandler = require('./middleware/404.js');
+
+app.use('/public', express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(router);
+app.use(errorHandler);
+
+module.exports = {
+    app, // Express app
+    start: port =>
+      app.listen(port, err => {
+        if (err) {
+          console.error('There was an error starting the server...');
+        } else {
+          console.log(`You are connected to the Express server on port ${port}`);
+        }
+        }),
+};
